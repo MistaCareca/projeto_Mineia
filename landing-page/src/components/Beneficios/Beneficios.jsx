b04c42d0906fba5890fa904bcba80a65638dc1ae
Caê <mcmlins17@gmail.com>
2025-02-08 22:38:40 -0300

    wip - beneficios.css(estrutura da página feita)

--- landing-page/src/components/Beneficios/Beneficios.jsx
+++ landing-page/src/components/Beneficios/Beneficios.jsx
@@ -7,14 +7,16 @@ function Beneficios(){
         <section className='beneficios'>
             <h1>Benefícios do E-Book:</h1>
             <div className='circulo'>
+                <div className='textos'>
                 <p>Fortalecimento de Relacionamento</p>
-                <p className='destaque'>Redução de Estresse</p>
+                <p className='destaque_beneficios'>Redução de Estresse</p>
                 <p>Melhoria da Comunicação</p>
                 <p>Equilíbrio entre Vida Pessoal e Profissional</p>
-                <p className='destaque'>Crescimento Espiritual a Dois</p>
-                <p className='destaque'>Aumenta a Confiança</p>
+                <p className='destaque_beneficios'>Crescimento Espiritual a Dois</p>
+                <p className='destaque_beneficios'>Aumenta a Confiança</p>
                 <p>Desenvolvimento Pessoal e Profissional</p>
-                <p className='destaque'>Melhoria da Vida Sexual</p>
+                <p className='destaque_beneficios'>Melhoria da Vida Sexual</p>
+                </div>
             </div>
             <img src={placeholder} alt="logo do livro" className='livro'/>
         </section>
