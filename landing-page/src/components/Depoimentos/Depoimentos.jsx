be19ebd41e462030dcd8a14005114dbde5661752
Caê <mcmlins17@gmail.com>
2025-02-11 17:36:36 -0300

    wip - carddep.css(estrutura e depoimentos prontos, incluir icon generico)

--- landing-page/src/components/Depoimentos/Depoimentos.jsx
+++ landing-page/src/components/Depoimentos/Depoimentos.jsx
@@ -6,9 +6,9 @@ function Depoimentos(){
         <div>
             <h1>Depoimentos dos nossos clientes:</h1>
             <div className='depoimentos'>
-                <CardDep nome= "Caitlyn" desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
-                <CardDep nome= "Violet" desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
-                <CardDep nome= "Jinx" desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
+                <CardDep nome= "Anônimo" desc = "Um ato de bravura! Para escrever essas palavras, aliceçadas na lei de Deus, precisa ter tido muito conhetcimento delas, e desejado vivê-las no íntimo da alma."/>
+                <CardDep nome= "Anônimo" desc = "O livro certamente terá grande impacto entre aqueles que buscam viver de maneira plena e alinhada com os ensinamentos de Deus. É uma obra pequena em tamanho mas rica em sabedoria, e que certamente pode crescer ainda mais em um futuro segundo volume."/>
+                <CardDep nome= "Anônimo" desc = "Que esse livro chegue nas mãos, nos corações e nas mentes que estejam abertas para receber esse despertar. Fiquei encantada, sobretudo por ter sido escrito por uma mulher!"/>
             </div>
         </div>
     )
