62f1d26eb903bb138a3f9d87bd3f902d1a570eac
Caê <mcmlins17@gmail.com>
2025-02-12 01:14:01 -0300

    feat - carddep.css

--- landing-page/src/components/CardDep/CardDep.jsx
+++ landing-page/src/components/CardDep/CardDep.jsx
@@ -1,11 +1,11 @@
 import './CardDep.css'
-import placeholder from '../../assets/placeholder.png'
+
 
 function CardDep(props){
     return(
     <div>
         {/*add depois de ter fotos {props.foto}*/}
-        <img src={placeholder} alt={props.nome}/>
+        <img src={props.foto} alt={props.nome}/>
         <h4>{props.nome}</h4>
         <p>{props.desc}</p>
     </div>)
--- landing-page/src/components/Depoimentos/Depoimentos.jsx
+++ landing-page/src/components/Depoimentos/Depoimentos.jsx
@@ -1,14 +1,15 @@
 import './Depoimentos.css'
 import CardDep from '../CardDep/CardDep'
+import placeholder from '../../assets/usuario-de-perfil.png'
 
 function Depoimentos(){
     return(
-        <div>
+        <div className='tit'>
             <h1>Depoimentos dos nossos clientes:</h1>
             <div className='depoimentos'>
-                <CardDep nome= "Anônimo" desc = "Um ato de bravura! Para escrever essas palavras, aliceçadas na lei de Deus, precisa ter tido muito conhetcimento delas, e desejado vivê-las no íntimo da alma."/>
-                <CardDep nome= "Anônimo" desc = "O livro certamente terá grande impacto entre aqueles que buscam viver de maneira plena e alinhada com os ensinamentos de Deus. É uma obra pequena em tamanho mas rica em sabedoria, e que certamente pode crescer ainda mais em um futuro segundo volume."/>
-                <CardDep nome= "Anônimo" desc = "Que esse livro chegue nas mãos, nos corações e nas mentes que estejam abertas para receber esse despertar. Fiquei encantada, sobretudo por ter sido escrito por uma mulher!"/>
+                <CardDep foto={placeholder} nome= "Anônimo" desc = "Um ato de bravura! Para escrever essas palavras, aliceçadas na lei de Deus, precisa ter tido muito conhetcimento delas, e desejado vivê-las no íntimo da alma."/>
+                <CardDep foto={placeholder} nome= "Anônimo" desc = "O livro certamente terá grande impacto entre aqueles que buscam viver de maneira plena e alinhada com os ensinamentos de Deus. É uma obra pequena em tamanho mas rica em sabedoria, e que certamente pode crescer ainda mais em um futuro segundo volume."/>
+                <CardDep foto={placeholder} nome= "Anônimo" desc = "Que esse livro chegue nas mãos, nos corações e nas mentes que estejam abertas para receber esse despertar. Fiquei encantada, sobretudo por ter sido escrito por uma mulher!"/>
             </div>
         </div>
     )
